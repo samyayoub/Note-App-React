c75f3b981433279c5c38cecd0e31bdba9eeb1496
Samy Ayoub <samyayoub@gmail.com>
2019-09-07 22:37:32 -0500

    Configure components based on state of application

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <List />
-      <Note />
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
+  render() {
+    const { showNote } = this.state;
+
+    return (
+      <div className="App">
+        <Nav />
+        { showNote ? <Note /> : <List /> }
+      </div>
+    );
+  }
 }
 
 export default App;
